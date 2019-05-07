d6953119d28231566c4b92a6cdcfbce5d5dc7548
Sujoy Das <sujoydas96>
2019-05-07 21:21:24 +0530

    Corrected Errors

--- Eular.c
+++ Eular.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<math.h>
-#include<conio.h>
 
 float f(float x, float y);
 
@@ -8,23 +7,23 @@ int main()
 {
   int i = 0;
   float x0, y0, x1, y1, xn, h;
-  printf("Enter the initial value of x and y :\n");
+  puts("Enter the initial value of x and y :");
   scanf("%f%f", &x0, &y0);
-  printf("Enter the step length :");
+  puts("Enter the step length :");
   scanf("%f", &h);
-  printf("Enter the value of x for which y is required :");
+  puts("Enter the value of x for which y is required :");
   scanf("%f", &xn);
 
   while (x0 <= xn)
   {
-    y1 += h * f(x0, y0);
-    x1 += h;
+    y1 = y0 + h * f(x0, y0);
+    x1 = x0 + h;
     x0 = x1;
     y0 = y1;
 
     printf("\nWhen x(%d) = %f\tThen y(%f) = %f", i++, x0, x0, y0);
   }
-  getch();
+
   return 0;
 }
 
